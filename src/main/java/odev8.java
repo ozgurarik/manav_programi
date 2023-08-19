750e6982ed9ca9a992552e43acc5d9b75cd4b5e8
ozgurarik <math.ozgurarik@gmail.com>
2023-08-19 11:28:27 +0300

    first commit

--- src/main/java/odev8.java
+++ src/main/java/odev8.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
 
 public class odev8 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Scanner armut=new Scanner(System.in);
+        Scanner armut = new Scanner(System.in);
         System.out.print("kaç kg Armut aldınız:");
-        double a=armut.nextDouble();
+        double a = armut.nextDouble();
 
-        Scanner elma=new Scanner(System.in);
+        Scanner elma = new Scanner(System.in);
         System.out.print("kaç kg elma aldınız:");
-        double e=elma.nextDouble();
+        double e = elma.nextDouble();
 
-        Scanner domates=new Scanner(System.in);
+        Scanner domates = new Scanner(System.in);
         System.out.print("kaç kg domates aldınız:");
-        double d=domates.nextDouble();
+        double d = domates.nextDouble();
 
-        double totalTutar=a*30+e*40+d*25;
-        System.out.print(" toplam tutar :"+totalTutar);
-
+        double totalTutar = a * 30 + e * 40 + d * 25;
+        System.out.print(" toplam tutar :" + totalTutar);
     }
 }
